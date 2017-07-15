c2e660d82c4f7e21c42c8533e2020c70fd7d3642
Kuno <tonnos@gmx.de>
2017-07-15 20:02:06 +0200

    remove now aailable countries

--- Server/src/Server.java
+++ Server/src/Server.java
@@ -45,13 +45,13 @@ public class Server
 			this.actions.put(AbstractAction.ACTION_SET_QUESTION, new ActionSetQuestion());
 			this.actions.put(AbstractAction.ACTION_CHOSE_COUNTRY, new ActionChoseCountry());
 
+//			state.addCountry(new Country("br", "Brasilien"));
+//			state.addCountry(new Country("jp", "Japan"));
+//			state.addCountry(new Country("tr", "Türkei"));
+//			state.addCountry(new Country("de", "Deutschland"));
 			state.addCountry(new Country("fr", "Frankreich"));
-			state.addCountry(new Country("tr", "Türkei"));
 			state.addCountry(new Country("ru", "Russland"));
-			state.addCountry(new Country("de", "Deutschland"));
-			state.addCountry(new Country("jp", "Japan"));
 			state.addCountry(new Country("eg", "Ägypten"));
-			state.addCountry(new Country("br", "Brasilien"));
 			state.addCountry(new Country("us", "USA"));
 		}
 
