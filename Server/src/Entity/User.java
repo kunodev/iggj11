8f0354770eae68568ab1b840dccdf685bf55ea5b
Daniel Gerards <daniel.gerards@innogames.com>
2017-07-15 13:22:43 +0200

    - added user colors

--- Server/src/Entity/User.java
+++ Server/src/Entity/User.java
@@ -9,10 +9,13 @@ public class User implements JSONSerializable
 	static final String ROLE_GAME_MASTER = "gameMaster";
 	static final String ROLE_GAME_PLAYER = "player";
 
+	private final String[] colors = {"#ff1e25", "#1e74ff", "#38ce25", "#ede923", "#ed2295", "#ed9822", "#22dced"};
+
 	private int    id;
 	private String userName;
 	private int    points;
 	private String role;
+	private String color;
 
 	public User(int id, String userName)
 	{
@@ -20,15 +23,17 @@ public class User implements JSONSerializable
 		this.userName = userName;
 		this.points = 0;
 		this.role = this.id == 1 ? ROLE_GAME_MASTER : ROLE_GAME_PLAYER;
+		this.color = this.colors[id % colors.length];
 	}
 
 	public JSONAware toJSON()
 	{
 		JSONObject obj = new JSONObject();
-		obj.put("id", id);
-		obj.put("name", userName);
-		obj.put("points", points);
-		obj.put("role", role);
+		obj.put("id", this.id);
+		obj.put("name", this.userName);
+		obj.put("points", this.points);
+		obj.put("role", this.role);
+		obj.put("color", this.color);
 
 		return obj;
 	}
